e73961b33c77411ddb7a1502f8d79e9f52c42b5b
KevinTranbar <kt222sx@student.lnu.se>
2025-10-10 11:40:37 +0200

    Task 1 : 7

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Task4 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int [] age = new int [10]; //max 10 integers
+        int group1 = 0;
+        int group2 = 0;
+        int group3 = 0;
+
+        System.out.println("Enter 10 people's age: ");
+        for (int i = 0; i < age.length; i++) { //loops input into age[i]
+            age[i] = scanner.nextInt();
+
+            if (age[i] < 18) { //checks the different ages and sorts into groups
+                group1++;
+            } else if (age[i] <= 65) {
+                group2++;
+            } else group3++;
+
+        }
+
+        System.out.println("<18: " + group1 + " people");
+        System.out.println("18-65: " + group2 + " people");
+        System.out.println("<65: " + group3 + " people");
+
+    }
 }
--- src/Task5.java
+++ src/Task5.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Task5 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
 }
