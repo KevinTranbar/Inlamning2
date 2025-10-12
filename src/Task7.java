e307e09f010da09bf560e423903f8ebd7160df87
KevinTranbar <kt222sx@student.lnu.se>
2025-10-12 17:44:07 +0200

    1 : 7

--- src/Task7.java
+++ src/Task7.java
@@ -1,2 +1,17 @@
 public class Task7 {
+
+    public static int[][] rotateMatrix(int[][] matrix) {
+
+
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+
+        rotateMatrix(new int[][]{ {1, 2, 3},
+                                  {4, 5, 6},
+                                  {7, 8, 9} });
+
+    }
 }
