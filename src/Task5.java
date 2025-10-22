fe817dc367aa0867871b6ef9919fbb3594da4f27
KevinTranbar <kt222sx@student.lnu.se>
2025-10-22 02:55:03 +0200

    1 : 7

--- src/Task5.java
+++ src/Task5.java
@@ -1,33 +1,23 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Task5 {
 
-    public static ArrayList<ArrayList<Integer>> incSubArrays(int[] arr) {
+    public static void incSubArrays(int[] subArray) {
 
-        ArrayList<ArrayList<Integer>> incSubArrays = new ArrayList<>(); //arrayList of arrayLists with int datatype, will hold all arraylists of increasing subarrays
-        ArrayList<Integer> subArrays = new ArrayList<>(); //arraylist, will hold temporary list
+        for (int i = 0; i < subArray.length - 1; i++) { //loop through array, checking for increasing subarrays
 
-        for (int i = 0; i < arr.length; i++) { //loop, check for increasing subarray through all index 1 by 1, then add to incSubArrays
-            subArrays.clear(); //clear list, prepare for next check
-            subArrays.add(arr[i]); //always adds first num (potential increasing subarray)
+            if (subArray[i] < subArray[i + 1]) { //start of an increasing subarray found -> print the first element
+                System.out.print(subArray[i]);
 
-            while (i + 1 < arr.length && arr[i] < arr[i + 1]) { //loop, first part hinders going past end of array (going out of bounds), second part checks for increasing subarray
-                /*
-                alternative method - adds first num here if list is empty and then adds the second if subarray is increasing
-                if (subArrays.isEmpty()) subArrays.add(arr[i]);
-                subArrays.add(arr[i + 1]);
-                */
-                i++; //skips first num because already added
-                subArrays.add(arr[i]); //adds next num if subarray is increasing
-            }
+                while (i + 1 < subArray.length && subArray[i] < subArray[i + 1]) { //continue while next element is greater -> increment by one(skip first elem in first loop) and print rest elements
+                    i++;
+                    System.out.print(", " + subArray[i]);
+                }
 
-            if (subArrays.size() > 1) { //if list is longer than 1 -> add to incSubArrays
-                incSubArrays.add(new ArrayList<>(subArrays)); //all increasing subarrays
+                System.out.println(); //print newline after finishing the current increasing subarray, only works if subArray is increasing
+
             }
         }
-
-        return incSubArrays;
     }
 
     public static void main(String[] args) {
@@ -43,8 +33,8 @@ public class Task5 {
             intArray[i] = Integer.parseInt(stringArray[i].trim());
         }
 
-        System.out.println("Increasing subarrays are: " + incSubArrays(intArray));
+        System.out.println("Increasing subarrays are:");
+        incSubArrays(intArray);
 
-        scanner.close();
     }
 }
